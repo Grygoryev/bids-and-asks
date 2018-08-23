b50fbf1139031a6b46d78e50d476afb2e7b988c4
Nickel 13 <nikesushko@gmail.com>
2018-08-23 19:40:33 +0300

    uploading to git

---
+++ build/js/index.js
@@ -0,0 +1,66 @@
+
+$.getJSON('../data.json', function(data) {         
+  console.log(data);
+  const bids = data.bids;
+  const asks = data.asks;
+
+  let totalBids = 0;
+  let totalAsks = 0;
+
+  for (let i = 0; i < bids.length; i++) {
+    totalBids += +bids[i].quantity;
+    let totalLevelWidth = (totalBids / 7) + 'px';
+
+    $('#bids').append(
+      '<div class="bids-body__line">' + 
+        '<div class="bids-body__count">' +
+          bids[i].numberOfOrders + 
+        '</div>' +  
+        '<div class="bids-body__total">' +
+          +totalBids.toFixed(3) + 
+        '</div>' +  
+        '<div class="bids-body__amount">' + 
+          bids[i].quantity +
+        '</div>' +
+        '<div class="bids-body__price">' + 
+        '<div class="bids-body__level-total" style=" width:' + totalLevelWidth + ';"  id="bids' + i + ' "></div>' +
+          bids[i].price +
+        '</div>' +
+      '</div>' 
+    )
+  }
+
+  for (let i = 0; i < asks.length; i++) {
+    totalAsks += +asks[i].quantity;
+    let totalLevelWidth = (totalAsks / 7) + 'px';
+    
+    $('#asks').append(
+      '<div class="asks-body__line">' + 
+
+        '<div class="asks-body__price">' + 
+          '<div class="asks-body__level-total" style=" width:' + totalLevelWidth + ';" id="asks' + i + ' "></div>' +
+          asks[i].price +
+        '</div>' +  
+        '<div class="asks-body__amount">' + 
+          asks[i].quantity +
+        '</div>' +
+        '<div class="asks-body__total">' +
+          +totalAsks.toFixed(3) + 
+        '</div>' +
+        '<div class="asks-body__count">' +
+          asks[i].numberOfOrders + 
+        '</div>' +
+
+      '</div>' 
+    )
+  }
+});
+
+$('#bids').scroll(function() {
+  $('#asks').scrollTop($('#bids').scrollTop());
+});
+
+$('#asks').scroll(function() {
+  $('#bids').scrollTop($('#asks').scrollTop());
+});
+
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const   gulp = require('gulp'),
+        pug = require('gulp-pug'),
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        del = require('del'),
+        minify = require("gulp-csso"),
+        rename = require("gulp-rename"),
+        imagemin = require("gulp-imagemin"),
+        server = require('browser-sync');
+
+
+gulp.task('pug', () => {
+    return gulp.src('src/pug/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('build'))
+})
+
+gulp.task('sass', () => {
+    return gulp.src('src/sass/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(minify())
+        .pipe(rename("main.min.css"))
+        .pipe(gulp.dest('build/css'))
+})
+
+gulp.task('js', () => {
+    return gulp.src('src/js/*.js')
+        .pipe(gulp.dest('build/js'))
+})
+
+gulp.task('img', () => {
+    return gulp.src('src/img/**/*')
+        .pipe(imagemin([
+            imagemin.optipng({optimisationLevel: 5}),
+            imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest('build/img'))
+})
+
+gulp.task('json', () => {
+    return gulp.src('src/*.json')
+        .pipe(gulp.dest('build/'))
+})
+
+gulp.task('server', () => {
+    server.init({
+        server: {
+          baseDir: 'build'
+        },
+        notify: true
+      })
+})
+
+gulp.task('watch', function () {
+    gulp.watch('src/sass/**/*.*', gulp.series('sass'));
+    gulp.watch('src/pug/**/*.*', gulp.series('pug'));
+    gulp.watch('src/js/**/*.*', gulp.series('js'));
+  
+    gulp.watch('build/**/*.*').on('change', server.reload);
+  });
+
+  gulp.task('clean', function () {
+    return del('build');
+  });
+  
+  gulp.task('build', gulp.series('clean', gulp.parallel('pug', 'sass', 'img', 'js', 'json')));
+  
+  gulp.task('serve', gulp.parallel('watch', 'server'));
+  
+  gulp.task('dev', gulp.series('build', 'serve'));
